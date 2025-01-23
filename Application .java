0932cb47e86d530e3de0760061f383d7ca0f4539
taslimshaik <emailtaslimshaik18800@gmail.com>
2025-01-23 16:30:06 +0530

    added by vivek

--- Application .java
+++ Application .java
@@ -15,3 +15,18 @@
         System.out.println("The sum is: " + result);
     }
 }
+# feature-202 change by vivek  for the module reports on oc 2024
+
+  // File: Animal.java
+
+// A class with protected access specifier
+public class Animal {
+
+    // Protected field
+    protected String species = "Unknown"; // Initialize with a default value
+
+    // Protected method
+    protected void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
