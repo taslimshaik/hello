d985cfe462bc2b3b8cc88e1941def3e7126c5276
taslimshaik <emailtaslimshaik18800@gmail.com>
2025-01-23 14:14:58 +0530

    added application .java

--- Application .java
+++ Application .java
@@ -1,4 +1,25 @@
-#HDFC application file
+public class MyClass {
+
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}#HDFC application file
   public class SimpleMethodExample {
 
     // Method that takes two integers and returns their sum
